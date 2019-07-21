57f7fa30837617d9edc22016da7e273a8aa856a5
Jonah Dahlquist <jonah@jonah.name>
2019-07-20 20:15:18 -0500

    Restored full UDP server, compiles with tweaks to w5500 lib

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@
 
 extern crate panic_halt;
 
-use embedded_hal::spi::FullDuplex;
-use embedded_hal::digital::v2::OutputPin;
-use arduino_uno::spi::{Settings, DataOrder, SerialClockRate, SerialClockPolarity, SerialClockPhase};
+use embedded_hal::digital::v1_compat;
+use embedded_hal::blocking::delay::DelayMs;
+use w5500::{W5500, OnWakeOnLan, OnPingRequest, ConnectionType, ArpResponses, MacAddress, IpAddress, Socket, IntoUdpSocket, Udp};
 
 #[no_mangle]
 pub extern fn main() -> () {
     let dp = arduino_uno::Peripherals::take().unwrap_or_else(|| panic!());
+    let mut delay = arduino_uno::Delay::new();
     let mut pins = arduino_uno::Pins::new(
         dp.PORTB,
         dp.PORTC,
@@ -24,37 +25,65 @@ pub extern fn main() -> () {
         57600,
     );
 
+    ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").unwrap();
+
+
     let mut spi = arduino_uno::spi::Spi::new(
         dp.SPI,
         pins.d13.into_output(&mut pins.ddr),
         pins.d11.into_output(&mut pins.ddr),
         pins.d12.into_pull_up_input(&mut pins.ddr),
-        Settings {
-            data_order: DataOrder::MostSignificantFirst,
-            clock: SerialClockRate::OscfOver4,
-            clock_polarity: SerialClockPolarity::IdleLow,
-            clock_phase: SerialClockPhase::SampleLeading,
-        },
+        arduino_uno::spi::Settings::default(),
     );
 
-    let mut cs_w5500 = pins.d10.into_output(&mut pins.ddr);
+    let mut cs_w5500 = v1_compat::OldOutputPin::new(pins.d10.into_output(&mut pins.ddr));
 
-    // begin frame
-    cs_w5500.set_low().unwrap_or_else(|_| panic!());
+    let mut w5500 = W5500::with_initialisation(
+        &mut cs_w5500,
+        &mut spi,
+        OnWakeOnLan::Ignore,
+        OnPingRequest::Respond,
+        ConnectionType::Ethernet,
+        ArpResponses::Cache
+    ).ok();
 
-    let version = spi
-        // 16-bit offset
-        .send(0)
-        .and_then(|_| spi.send(0x39))
-        // control byte: Common register, read mode, variable-length mode
-        .and_then(|_| spi.send(0b00000000))
-        // push read byte from chip
-        .and_then(|_| spi.send(0))
-        .and_then(|_| spi.read())
-        .unwrap_or_else(|_| panic!());
+    if let Some(ref mut w5500) = w5500 {
+        ufmt::uwriteln!(&mut serial, "initialized\r").unwrap();
+        let mut w5500 = w5500.activate(&mut spi).unwrap_or_else(|_| panic!());
+        ufmt::uwriteln!(&mut serial, "activated\r").unwrap();
+        w5500.set_mac(MacAddress::new(0x02, 0x01, 0x02, 0x03, 0x04, 0x05)).unwrap_or_else(|_| panic!());
+        w5500.set_ip(IpAddress::new(192, 168, 86, 30)).unwrap_or_else(|_| panic!());
+        w5500.set_subnet(IpAddress::new(255, 255, 255, 0)).unwrap_or_else(|_| panic!());
+        w5500.set_gateway(IpAddress::new(192, 168, 86, 1)).unwrap_or_else(|_| panic!());
 
-    // end frame
-    cs_w5500.set_high().unwrap_or_else(|_| panic!());
+        let socket0 = w5500.take_socket(Socket::Socket0).unwrap_or_else(|| panic!());
+        let udp_server_socket = (&mut w5500, socket0).try_into_udp_server_socket(1234).ok();
 
-    ufmt::uwriteln!(&mut serial, "Version: {} (should be 4)\r", version).unwrap_or_else(|_| panic!());
+        let mut buffer = [0u8; 256];
+        ufmt::uwriteln!(&mut serial, "set up\r").unwrap();
+
+        if let Some(ref socket) = udp_server_socket {
+            ufmt::uwriteln!(&mut serial, "took socket\r").unwrap();
+            let mut udp = (&mut w5500, socket);
+
+            loop {
+                if let Ok(Some((ip, port, length))) = udp.receive(&mut buffer[..]) {
+                    ufmt::uwriteln!(&mut serial, "received\r").unwrap();
+                    let (_request_buffer, response_buffer) = buffer.split_at_mut(length);
+                    response_buffer[0] = 115;
+                    response_buffer[1] = 117;
+                    response_buffer[2] = 112;
+                    let response_length = 3;
+                    udp.blocking_send(&ip, port, &response_buffer[..response_length]).unwrap_or_else(|_| panic!());
+                } else {
+                    ufmt::uwriteln!(&mut serial, "could not receive\r").unwrap();
+                }
+                delay.delay_ms(1000);
+            }
+        } else {
+            ufmt::uwriteln!(&mut serial, "could not take socket\r").unwrap();
+        }
+    } else {
+        ufmt::uwriteln!(&mut serial, "could not initialize\r").unwrap();
+    }
 }
